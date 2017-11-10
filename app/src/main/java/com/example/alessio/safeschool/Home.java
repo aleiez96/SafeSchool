9332d2289678ad9c8ce616295336e86e47dabb51
alessio <858050@stud.unive.it>
2017-11-10 16:49:45 +0100

    aggiunta drawe con mappa

--- app/src/main/java/com/example/alessio/safeschool/Home.java
+++ app/src/main/java/com/example/alessio/safeschool/Home.java
@@ -3,6 +3,7 @@ package com.example.alessio.safeschool;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -28,11 +29,17 @@ public class Home extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),
-                        MapsActivity.class
+                        Drawer.class
                 );
 
                 startActivity(intent);
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
     }
 }
